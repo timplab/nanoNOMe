8d99c6d028e499f7feb64862c17b94396867ded7
isaclee <ilee29@jhmi.edu>
2018-08-01 21:52:48 -0400

    sniffles

---
+++ analysis/data_parse/180801_sniffles.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sniffles=~/Code/Sniffles*/bin/sniffles*/sniffles
+root=/dilithium/Data/Nanopore/projects/nomeseq/analysis/pooled
+bases="GM12878 MCF10A MCF7 MDAMB231"
+#base="{}"
+bam=$root/bam/$base.pooled.bam
+outdir=$root/sv
+[ -e $outdir ]||mkdir $outdir
+svout=$outdir/$base.sniffles.vcf
+if [ "$1" == "vcf" ];then
+  echo "generating vcf"
+  for base in $bases; do
+    bam=$root/bam/$base.pooled.bam
+    svout=$outdir/$base.sniffles.vcf
+    $sniffles -m $bam -s 3 -n 30 \
+      -v $svout --genotype --cluster --tmp_file $outdir/$base.tmp &> $svout.log
+  done
+fi
+bedout=$outdir/$base.sniffles.bed
+if [ "$1" == "bed" ];then
+  echo "generating bedpe"
+  for base in $bases; do
+    bam=$root/bam/$base.pooled.bam
+    svout=$outdir/$base.sniffles.bed
+    $sniffles -m $bam -s 3 -n 30 \
+      -v $svout --genotype --cluster --tmp_file $svout.tmp &> $svout.log
+  done
+fi
