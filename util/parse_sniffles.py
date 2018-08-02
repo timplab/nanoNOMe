9e5bb60afa125b34b96c06d0acfbc733c2e05895
isaclee <ilee29@jhmi.edu>
2018-08-02 15:05:15 -0400

    sniffles parser

--- util/parse_sniffles.py
+++ util/parse_sniffles.py
@@ -14,60 +14,70 @@ def parseArgs():
             required=False, default=sys.stdin,help="input vcf file path (or stdin)")
     parser.add_argument('-o', '--output', nargs='?', type=argparse.FileType('w'),
             required=False, default=sys.stdout,help="output file path (or stdout)")
-    parser.add_argument('what',nargs='+',help="element(s) to extract")
+    parser.add_argument('what',help="element to extract")
     args = parser.parse_args()
     return args
 
-def parsewhats(whats) :
-    outwhats=list()
-    if "coords" in whats :
-        outwhats.append("chrom")
-        outwhats.append("start")
-        outwhats.append("end")
-    if "svtype" in whats :
-        outwhats.append("svtype")
-    return outwhats
+class SnifflesEntry :
+    def __init__(self,line) :
+        self.line=line.strip()
+        self.fields=self.line.split("\t")
+        (self.chrom,self.pos,self.id,self.ref, 
+                self.type,self.qual,self.filter,self.infostring,
+                self.format,self.genotype) = self.fields
+        self.pos = int(self.pos)
+        self.parseinfo()
+        self.parsegenotype()
+    def parseinfo(self) :
+        self.infofields = [ x.split("=") for x in self.infostring.strip().split(";")]
+        self.info = dict()
+        self.info["CONFIDENCE"] = self.infofields[0][0]
+        for entry in self.infofields[1:] :
+            self.info[entry[0]] = entry[1]
+        self.info["END"] = int(self.info["END"])
+    def parsegenotype(self) :
+        self.allele = self.genotype.split(":")[0]
+        self.num_against = int(self.genotype.split(":")[1])
+        self.num_for = int(self.genotype.split(":")[2])
+        self.coverage = self.num_against+self.num_for
+    def test(self,what):
+        if what == "multiregion" :
+            if ( self.type == "<TRA>" and
+                    self.coverage > 10 and
+                    self.coverage < 100 and
+                    self.allele == "0/1" ) :
+                return True
+    def printBedpe(self,name,out) :
+        n=500
+        print("\t".join([str(x) for x in [self.chrom,self.pos-n,self.pos+n,
+            self.info["CHR2"],self.info["END"]-n,self.info["END"]+n,
+            name,".",".",self.info["RNAMES"]]]),file=out)
+    def printBed(self,name,out) :
+        n=500
+        print("\t".join([
+            str(x) for x in [
+                self.chrom,self.pos-n,self.pos+n,name,
+                ".",".",self.info["RNAMES"]]
+            ]),file=out)
 
-def findelement(s,what,delimiter) :
-    if delimiter == ";" :
-        firststring=what.upper()+"=" 
-        endstring=";"
-    try:
-        start = s.rindex( firststring ) + len( firststring )
-        end = s.index( endstring, start )
-        return s[start:end]
-    except ValueError:
-        return ""
-
-
-def getwhats(line,whats):
-    out=list()
-    fields=line.strip().split("\t")
-    if "chrom" in whats :
-        if "chr" in fields[0] :
-            chrom=fields[0]
-        else : chrom="chr"+fields[0]
-        out.append(chrom)
-    if "start" in whats :
-        out.append(fields[1])
-    if "end" in whats :
-        out.append(findelement(fields[7],"end",";"))
-    if "svtype" in whats :
-        out.append(fields[4])
-    return out
-
-def printBed(outlist,out):
-    outlist.insert(4,".")
-    outlist.insert(5,".")
-    print("\t".join(outlist),file=out)
+        print("\t".join([
+            str(x) for x in [
+                self.info["CHR2"],self.info["END"]-n,self.info["END"]+n,
+                name,".",".",self.info["RNAMES"]]
+            ]),file=out)
 
 if __name__=="__main__":
     args=parseArgs()
 #    print(args)
 #    sys.exit()
-    whats=parsewhats(args.what)
+    # removing commented lines
+    cmtflag = 1
+    while cmtflag == 1 :
+        line = args.input.readline()
+        if line[0] != "#" : cmtflag = 0
+    i = 1
     for line in args.input :
-        for what in whats :
-            outlist=getwhats(line,whats)
-            printBed(outlist,out=args.output)
-                
+        entry = SnifflesEntry(line)
+        if entry.test(args.what) : 
+            entry.printBed(i,args.output)
+            i+=1
