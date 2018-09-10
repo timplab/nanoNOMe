c1456b10a7c7d4b7dfcb74400f5368da92a57022
isaclee <ilee29@jhmi.edu>
2018-09-10 11:03:11 -0400

    igv,mappability

--- analysis/igv/180823_igv_test.sh
+++ analysis/igv/180823_igv_test.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-cells="MCF10A" #MCF7 MDAMB231"
+cells="GM12878"
 root=/dilithium/Data/Nanopore/projects/nomeseq/analysis
-bed=$root/plots/repeats/LINE_regions.bed
-reg=testset
+bed=$root/database/hg38/hg38_repeats_LINE_long.bed
+reg=LINE_long
 pooldir=$root/pooled
+script=../../nanopolish/convertBam.py
 for cell in $cells;do
   bam=$pooldir/bam/$cell.pooled.bam
   mbeddir=$pooldir/methylation/methbyread_all
   cpg=$mbeddir/$cell.cpg.pooled.meth.bed.gz
   gpc=$mbeddir/$cell.gpc.pooled.meth.bed.gz
-  script=../../nanopolish/convertBam.py
-  log=$pooldir/igv/$cell.$reg.log
-  out=$pooldir/igv/$cell.$reg.bam
 
-  python -u $script -v -t 8 -b $bam -c $cpg -g $gpc -r $bed -o $out 2> $log
-  outsort=$pooldir/igv/$cell.$reg.sorted.bam
-  samtools sort -o $outsort $out
-  samtools index $outsort
+  if [ "$1" == "meth" ];then
+    log=$pooldir/igv/$cell.$reg.log
+    out=$pooldir/igv/$cell.$reg.sorted.bam
+    com="python -u $script -v -t 8 \
+      -b $bam -c $cpg -g $gpc -r $bed 2> $log |\
+      samtools sort -o $out"
+  fi
+  if [ "$1" == "original" ];then
+    out=$pooldir/igv/$cell.$reg.original.sorted.bam
+    com="samtools view -@ 10 -hb -L $bed $bam |\
+      samtools sort -@ 10 -o $out"
+  fi
+  eval $com
+  samtools index $out
 done
--- script/README.md
+++ script/README.md
@@ -2,3 +2,5 @@
 used to convert the nanopolish output methylation call tsv files into bed-style formats
 ### readlevelNDR.py 
 uses output from `bedtools intersect` to perform NDR searching
+### bigWigToBedGraph
+taken from UCSC genome browser utilities
